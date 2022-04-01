a93a5aba4d53049a067389cd1fbba944b5077e8c
Jacob <Jacob.McCann214@gmail.com>
2022-04-01 08:40:38 -0500

    All functionalities working for core deliverables

--- index.js
+++ index.js
@@ -1,44 +1,83 @@
-// const BASE_URL = 'https://zenquotes.io/api/quotes'
+document.addEventListener('DOMContentLoaded', () => {
+
+    const BASE_URL = 'https://type.fit/api/quotes'
+
+    // GENERATE RANDOM QUOTE:
+    fetch(BASE_URL)
+        .then((res) => res.json())
+        .then(data => {
+            getRandQuote(data);
+            dropDownMenu(data)
+        })
 
 
-// fetch zen quotes api
-
-// fetch(BASE_URL)
-// .then(resp => resp.json())
-// .then(data => {
-//     console.log(data)
-// })
-
-
-// Create a function to render quotes to DOM and append
-
-
-
-// Add event listeners comments,
-// Click like button
-// submit a comment
-// change drop down menu
+    function getRandQuote(data){
+        const randomIndex = Math.floor(Math.random()*data.length);
+            document.getElementById("quote").textContent = data[randomIndex].text;
+    }
 
 
 
+    // DROP DOWN MENU:
+    const dropEl = document.querySelector('#quote-dropdown')
+
+    const dropDownMenu = (data) => {
+        const myIndex = 4;
+        const myIndex2 = 6;
+        const myIndex3 = 17;
+
+        dropEl.addEventListener('change', e => {
+            if (e.target.value == 'a'){
+                // console.log(e.target.value)
+                return document.getElementById("quote").textContent = data[myIndex].text;
+            } else if (e.target.value == 'b'){
+                return document.getElementById("quote").textContent = data[myIndex2].text;
+            } else if (e.target.value = 'c'){
+                return document.getElementById("quote").textContent = data[myIndex3].text;
+            }
+        })
+    };
 
 
-// SUBMIT A COMMENT:
-const commentForm = document.querySelector('#comment-form')
-const commentList = document.querySelector('#comment-list')
-
-commentForm.addEventListener('submit', newComment)
 
 
-function newComment(e, comments) {
-    e.preventDefault();
-
-    let li = document.createElement('li')
-    li.textContent = e.target.addComment.value
-
-    commentList.append(li)
-}
+    // SUBMIT A COMMENT:
+    // const postForm = document.querySelector('#post-container')
+    const formContainer = document.querySelector('#form-container')
+    const commentList = document.querySelector('#comment-list')
+    const commentContainer = document.querySelector('#comment-container')
+    formContainer.addEventListener('submit', newCommentAndlikes)
 
 
+    function newCommentAndlikes (e) {
+        e.preventDefault();
+        const div = document.createElement('div')
+        div.id = "commentsAndLikes-container"
 
-// console.log('test')
+        let li = document.createElement('li')
+        li.textContent = e.target.addComment.value
+
+        const commentLikesNum = document.createElement('h3')
+        commentLikesNum.textContent = "0"
+
+        const likesBttn = document.createElement("button")
+        likesBttn.className = 'like-bttn'
+        likesBttn.textContent = "♥"
+
+        likesBttn.addEventListener('click', () => increaseLikes(commentLikesNum))
+
+
+        li.appendChild(commentLikesNum)
+        li.appendChild(likesBttn)
+        commentList.append(li)
+        commentContainer.appendChild(commentList)
+
+        formContainer.reset()
+    }
+
+    function increaseLikes(commentLikesNum){
+        // Increments number of likes
+        ++commentLikesNum.textContent
+      }
+
+})
